a9360df4062ce2c5b0221f76863fd772e739cb18
Howard Riddiough <howardriddiough@howworkmac.local>
2018-02-19 17:32:29 +0100

    now uses yahoo reader instead of quandl

--- predict.py
+++ predict.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import finance_data as fd
+import yahoo_reader as yr
 import processing as ps
 from sklearn.model_selection import RandomizedSearchCV, train_test_split, ShuffleSplit
 from sklearn.ensemble import RandomForestRegressor
@@ -9,24 +9,24 @@ import plots
 
 #%%
 # Get Stock Data
-fd = fd.finance_data()
-df = fd.getData()
+yr = yr.finance_data()
+df = yr.getData()
 
 #%%
 # Process for predictions
 df = ps.prepData(df)
 df = ps.genFeatures(df)
 df = ps.featureProcessing(df)
-df = ps.genTarget(df)
+df = ps.genTargets(df)
 
 #%%
 # Define X and y
 X = df[['open_sc','high_sc','low_sc','close_sc','volume_sc',
-        'year_sc',#'dom_sc','weekday_sc','month_sc',
+        'year_sc',
         'ce_month_x','ce_month_y','ce_dow_x','ce_dow_y','ce_doy_x','ce_doy_y',
         'high_low_sc','open_min1_sc','close_min1_sc','volume_min1_sc',
         'high_low_min1_sc','open_min5_sc','close_min5_sc','volume_min5_sc',
-        'high_low_min5_sc','open_cagr_sc','close_cagr_sc','code_en']]
+        'high_low_min5_sc','open_cagr_sc','close_cagr_sc','ticker_en']]
 y = df['regressor_y']
 
 #%%
@@ -61,15 +61,13 @@ plots.barFeatureImportance(feature_importances=rf.feature_importances_,
 
 #%%
 # Draw Histogram of errors on test and train
-plots.dualHist(rf.predict(X_test) - y_test, 
-               rf.predict(X_train) - y_train,
-               np.sqrt(np.mean((rf.predict(X_test) - y_test) ** 2)).astype(int), 
-               np.sqrt(np.mean((rf.predict(X_train) - y_train) ** 2)).astype(int), 
-               'test', 
-               'train', 
-               title='Prediction Error: test vs train',
-               xlabel='$',
-               hist_range=[-15,15])
+plots.dualHist(errors1 = rs.predict(X_test) - y_test, 
+               errors2 = rs.predict(X_train) - y_train,
+               label1 = 'test', 
+               label2 = 'train', 
+               title = 'Prediction Error: test vs train',
+               xlabel = '$',
+               hist_range = [-15,15])
 
 #%%
 title = 'Learning Curves (Random Forest Regression)'
@@ -81,3 +79,19 @@ plt = plots.plot_learning_curve(rf, title, X, y, cv=cv, n_jobs=5)
 
 plt.show()
 plt.close()
+
+#%%
+# Use classifier
+from sklearn.ensemble import RandomForestClassifier
+
+y = df['classification_y']
+
+# Split df
+X_train, X_test, y_train, y_test = train_test_split(X, y, 
+                                                    test_size=0.8,
+                                                    shuffle=False)
+
+rc = RandomForestClassifier(n_jobs=2)
+
+rc.fit(X_train, y_train)
+
