141ce4033abcb8b3dd57e9e26e6b9b278e9bc15a
Howard Riddiough <howardriddiough@howworkmac.home>
2018-03-10 22:58:33 +0100

    adding new function to compose email

--- pygmail.py
+++ pygmail.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 import os
 import sys
 import smtplib
@@ -11,11 +13,16 @@ from email.mime.text import MIMEText
 # email commaspace
 COMMASPACE = ', '
 # Load gmail accpunt settings from yml
-gm = yaml.load(open('./gmail_settings.yml'))
+gm = yaml.load(open('./configs/gmail_settings.yml'))
 # Define time now
 now = dt.datetime.now().strftime("%d-%m-%Y %H:%M")
+# Define today
+today = dt.datetime.now().strftime("%d-%m-%Y")
+# List of recipients
+recipients = ['machineearning2018@gmail.com', 'erwin.lodder@gmail.com',
+              'howardriddiough@gmail.com']
 
-def send_mail(recipients=['machineearning2018@gmail.com'], 
+def send_mail(recipients=recipients, 
              subject=None,
              attachments=[], 
              body=None):
@@ -35,17 +42,17 @@ def send_mail(recipients=['machineearning2018@gmail.com'],
     pswrd = gm['password']
     
     # Create the enclosing (outer) message
-    msg = MIMEMultipart()
+    outer = MIMEMultipart()
     
     # Add subeject to email if subject is not None
     if subject is not None:
-        msg['Subject'] = subject
-    msg['To'] = COMMASPACE.join(recipients)
-    msg['From'] = sender
+        outer['Subject'] = subject
+    outer['To'] = COMMASPACE.join(recipients)
+    outer['From'] = sender
     
     # Add message to email if body is not None
     if body is not None:
-        msg.attach(MIMEText(body, 'plain'))
+        outer.attach(MIMEText(body, 'plain'))
 
     # Add the attachments to the message
     for file in attachments:
@@ -56,12 +63,12 @@ def send_mail(recipients=['machineearning2018@gmail.com'],
             encoders.encode_base64(msg)
             msg.add_header('Content-Disposition', 'attachment', 
                            filename=os.path.basename(file))
-            msg.attach(msg)
+            outer.attach(msg)
         except:
             print("Unable to open one of the attachments. Error: ", sys.exc_info()[0])
             raise
 
-    composed = msg.as_string()
+    composed = outer.as_string()
 
     # Send the email
     try:
@@ -76,3 +83,82 @@ def send_mail(recipients=['machineearning2018@gmail.com'],
     except:
         print("unable to send the email. error: ", sys.exc_info()[0])
         raise
+        
+def compose_email(expected_deltas : dict):
+    """
+    """
+    # Create email subject
+    subject = 'Expected Deltas Notification: ' + now
+    
+    # Create data Frame from dict
+    df = pd.DataFrame(list(expected_deltas.items()), 
+                      columns=['ticker', 'exp_delta'])
+    # Sort values by exp_delta
+    df = df.sort_values('exp_delta', ascending=False)
+    df = df.reset_index(drop=True)
+    # Round expected deltas
+    df['exp_delta'] = np.round(df.exp_delta, 3)
+    # Create String from email
+    body = 'Expected Deltas:'
+    
+    for idx, row in df.iterrows():
+        # If idx is one digit then add a space before
+        if len(str(idx + 1)) == 1:
+            num = ' ' + str(idx + 1)
+        else:
+            num = str(idx + 1)
+            
+        # If ticker is three letters long add a space before
+        if len(row.ticker) == 3:
+            tick = ' ' + row.ticker
+        else:
+            tick = row.ticker
+            
+        # If expected delta is postive add a space before
+        if row.exp_delta >= 0:
+            delta = ' ' + str(row.exp_delta)
+        else:
+            delta = str(row.exp_delta)
+            
+        # Create email body string
+        body = body + '\n' + num + '. ' + tick + ': ' + delta
+        
+    # Create list of attachments ranked by expected delta
+    files = os.listdir('./plots/' + today)
+    # List of tickers in dictionary
+    tickers_in_dict = list(expected_deltas.keys())
+    
+    # Create list of graphs that are only in tickers_in_dict list
+    graphs = [file for file in files if file[:4] in tickers_in_dict or file[:3] in tickers_in_dict]
+    # Select graph file if it is a compare_predictions or current_predictions
+    graphs = [file for file in graphs if file[-22:] == 'current_prediction.png' or file[-15:] == 'predictions.png']
+    
+    # Use pandas to sort list of graph names by expected delta
+    df_graphs = pd.DataFrame(graphs, columns=['filename'])
+    # Create ticker id in DataFrame
+    df_graphs['ticker'] = df_graphs.filename.str[:4].str.replace('_','')
+    # Use file to create graph type column
+    df_graphs['graph_type'] = df_graphs.filename.str[-15:]
+    
+    # Merge df and df_graphs
+    df_graphs = df_graphs.merge(df, on='ticker', how='inner')
+    # Sort df_graphs by expected delta
+    df_graphs = df_graphs.sort_values(['exp_delta','graph_type'])
+    
+    # Create list of attachments
+    attachments = df_graphs.filename.tolist()
+    attachments = ['./plots/' + today + '/' + attachment for attachment in attachments]
+    
+        
+    return subject, body, attachments
+
+"""
+# Create email body
+subject, body, attachments = compose_email(expected_deltas=invest_dict)
+
+# Send email
+send_mail(subject=subject,
+          attachments=attachments,
+          body=body)
+"""
+
