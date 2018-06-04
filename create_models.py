dab05d6635040cea995e0909f0066881c604bb77
BarkieGitter <erwin.lodder@gmail.com>
2018-06-04 21:23:50 +0200

    superneatcode

--- create_models.py
+++ create_models.py
@@ -12,114 +12,84 @@ import plotting
 import gc
 import pygmail
 from keras.models import Sequential, load_model
-#%%
+
 model_folder = './short_term_models/'
 
-if not os.path.exists(model_folder):
-    os.mkdir(model_folder)
-
-df_tickers = pd.read_csv('tickers_of_interest.csv',sep=';')[-200:]
-tickers = df_tickers['ticker'].tolist()
-tickers_done = utils.get_tickers_done(model_folder)
-tickers_to_do = [ticker for ticker in tickers if ticker not in tickers_done]
-
-good_stocks = ['ADMP', 'ACHV', 'SNMX', 'CBI', 'ASNA', 'AIRI', 'BCRX']
-good_window_lengths = [21,16,16,16,16,16,16]
-#True for analyse, False for prediction
-##
-run_model = True
-##
-current = [ 'ASUR', 'ATLC', 'AIRI', 'SNMX','ALSK', 'BASI', 'BCRX', 'CAMT', 'CENX', 'CGEN', 'CLWT', 
-                      'CRZO', 'DWSN', 'AXTI', 'ELTK', 'ESIO', 'EXAS']
-#current = ['ASNA', 'ASUR', 'ATLC', 'AIRI']
-current = ['CBI', 'CAMT', 'AMAG', 'ADMP', 'DAIO', 'GALT', 'GEN']
-yr = yahoo_reader.finance_data(tickers=tickers_to_do)
-df_main, tickers_to_do = yr.main()
-
+volatile_tickers = pd.read_csv('volatile_complete.csv',sep=';')
+volatile_tickers_list = volatile_tickers['ticker'].tolist()
+volatile_tickers_done = pd.read_csv('tickers_done_short_correct.csv')
+volatile_tickers_done_list = volatile_tickers_done['tickers'].tolist()
+volatile_tickers_to_complete = [item for item in volatile_tickers_list if item not in volatile_tickers_done_list]
+yr = yahoo_reader.finance_data(tickers=volatile_tickers_to_complete[:20])
+df_main = yr.get_fix_yahoo_data()
 #%%
-ticker_dict = {}
-#df_main = pd.read_csv('saved_stock_data.csv')
-# Prep data for LSTM model
-window_length = 21
+df_main = df_main[0]
+run_model=True
 days_ahead=1
 df_test = {}
 short_term_folder = './short_term_models/'
-
-for ticker in tickers_to_do:
-    initial_investment = 100.0
-    window_size = 12
+compare_investment = 300.
+for ticker in volatile_tickers_to_complete[:20]:
+    initial_investment = 50.0
+#    window_size = 12
     for window_length in [16]:
-
         df = df_main[df_main.ticker == ticker].reset_index(drop=True)
         df['volume'] = df['volume'].replace(0,1.0)
-        
-        if run_model:
-            df_p = pp.pre_process_data(df, window_length=window_length)
-            close_nmd_array = utils.series_to_ndarray(df_p,window_length, column='close_nmd')
-            volumne_nmd_array = utils.series_to_ndarray(df_p,window_length, column='volume_nmd')
-            high_nmd_array = utils.series_to_ndarray(df_p,window_length, column='high_nmd')
-            low_nmd_array = utils.series_to_ndarray(df_p,window_length, column='low_nmd')
-            day_number_array = utils.series_to_ndarray(df_p,window_length, column='day_number')
-            dates_array = utils.series_to_ndarray(df_p,window_length, column='date')
-            windows_array = utils.series_to_ndarray(df_p,window_length, column='window')
-            #closing price must go first 
-            combined_input = np.concatenate((close_nmd_array,volumne_nmd_array,high_nmd_array,low_nmd_array, day_number_array),axis=2)
-            x_train, y_train, x_test, y_test,  train_days, test_days, test_windows = utils.train_test_split(combined_input,combined_input.shape[2], dates_array, windows_array)
-       
-            investment, best_investment_dev = lstm_model.randomised_model_config(test_windows,
-                                                            df_p,
-                                                            test_days,
-                                                            combined_input.shape[2],
-                                                            window_length,
-                                                            ticker,
-                                                            df,
-                                                            days_ahead,
-                                                            x_train,
-                                                            y_train, 
-                                                            x_test,
-                                                            y_test,
-                                                            iterations=6,
-                                                            epochs=8)
-            gc.collect()    
-
-            if investment>initial_investment :
-                initial_investment = investment
-                window_size = window_length
-#            del model
-
-                
-            
-            
-    #    date_today = dt.datetime.now().strftime("%Y-%m-%d")
-#        date = '2018-04-25'
-#        model = load_model(date+'_'+ticker+'_model.h5')
-        ### predict tomorrow  
-        ##train data do something with it
-        
-        if not run_model:
-            window_length_pred = window_length - 1
-            df_pred = pp.pre_process_data(df[-window_length_pred:], window_length=window_length_pred)
-            df_pred = df_pred.reset_index(drop=True)
-            close_nmd_array_pred = utils.series_to_ndarray(df_pred,window_length_pred, column='close_nmd')
-            volumne_nmd_array_pred = utils.series_to_ndarray(df_pred,window_length_pred, column='volume_nmd')
-            high_nmd_array_pred = utils.series_to_ndarray(df_pred,window_length_pred, column='high_nmd')
-            low_nmd_array_pred = utils.series_to_ndarray(df_pred,window_length_pred, column='low_nmd')
-            day_number_array_pred = utils.series_to_ndarray(df_pred,window_length_pred, column='day_number')
-        
-            combined_input_pred = np.concatenate((close_nmd_array_pred,volumne_nmd_array_pred,high_nmd_array_pred,low_nmd_array_pred,day_number_array_pred),axis=2)
-            predicted_normalised = (model.predict(combined_input_pred)[0,0])
-            predicted = (predicted_normalised + 1.0)*df_pred.loc[0,'normaliser']
-            growth = predicted/df_pred.loc[len(df_pred)-1,'close']
-            ticker_dict[ticker] = growth
-#        #%%
-#    volatile_tickers_done = pd.read_csv('tickers_done_short.csv')
-#    volatile_tickers_done_lst = volatile_tickers_done['tickers'].tolist()
-#        
-#    volatile_tickers_done_lst.append(ticker)
-#    volatile_tickers_done_lst_window_length = volatile_tickers_done['window_length'].tolist()
-#    volatile_tickers_done_lst_window_length.append(window_size)
-#    df_temp = pd.DataFrame({'tickers':volatile_tickers_done_lst,'window_length':volatile_tickers_done_lst_window_length})
-#    df_temp.to_csv('tickers_done_short.csv')
+        df_p = pp.pre_process_data(df, window_length=window_length)
+        close_nmd_array = utils.series_to_ndarray(df_p,window_length, column='close_nmd')
+        volumne_nmd_array = utils.series_to_ndarray(df_p,window_length, column='volume_nmd')
+        high_nmd_close_array = utils.series_to_ndarray(df_p,window_length, column='high_nmd_close')
+        low_nmd_close_array = utils.series_to_ndarray(df_p,window_length, column='low_nmd_close')
+        open_nmd_close_array = utils.series_to_ndarray(df_p,window_length, column='open_nmd_close')
+        day_number_array = utils.series_to_ndarray(df_p,window_length, column='day_number')
+        dates_array = utils.series_to_ndarray(df_p,window_length, column='date')
+        windows_array = utils.series_to_ndarray(df_p,window_length, column='window')
+        #closing price must go first 
+        combined_input = np.concatenate((close_nmd_array,open_nmd_close_array,low_nmd_close_array,high_nmd_close_array,volumne_nmd_array, day_number_array),axis=2)
+        x_train, y_train, x_test, y_test, train_days, test_days, test_windows,train_windows_non_randomized,x_train_sim = utils.train_test_split(combined_input,combined_input.shape[2], dates_array, windows_array)
+       #%%
+        investment, best_investment_dev,params,margin,mcr = lstm_model.randomised_model_config(test_windows,
+                                                        df_p,
+                                                        test_days,
+                                                        train_days,
+                                                        train_windows_non_randomized,
+                                                        x_train_sim,
+                                                        combined_input.shape[2],
+                                                        window_length,
+                                                        ticker,
+                                                        df,
+                                                        days_ahead,
+                                                        x_train,
+                                                        y_train, 
+                                                        x_test,
+                                                        y_test,
+                                                        iterations=10)
+        gc.collect()    
+    if (investment/compare_investment)>1.00 :        
+        volatile_tickers_done = pd.read_csv('tickers_done_short_correct.csv')
+        volatile_tickers_done_lst = volatile_tickers_done['tickers'].tolist()
+        volatile_tickers_done_lst.append(ticker)
+        volatile_tickers_done_lst_margin = volatile_tickers_done['margin'].tolist()
+        volatile_tickers_done_lst_margin.append(margin)
+        volatile_tickers_done_lst_window_length = volatile_tickers_done['window_length'].tolist()
+        volatile_tickers_done_lst_window_length.append(window_length)
+        volatile_tickers_done_lst_mcr = volatile_tickers_done['mcr'].tolist()
+        volatile_tickers_done_lst_mcr.append(mcr)
+        df_temp = pd.DataFrame({'tickers':volatile_tickers_done_lst,'window_length':volatile_tickers_done_lst_window_length,'margin':volatile_tickers_done_lst_margin,'mcr':volatile_tickers_done_lst_mcr})
+        df_temp.to_csv('tickers_done_short_correct.csv')
+    elif (investment/compare_investment)<1.00 :  
+        volatile_tickers_done = pd.read_csv('tickers_done_short_correct.csv')
+        volatile_tickers_done_lst = volatile_tickers_done['tickers'].tolist()
+        volatile_tickers_done_lst.append(ticker)
+        volatile_tickers_done_lst_margin = volatile_tickers_done['margin'].tolist()
+        volatile_tickers_done_lst_margin.append(np.NaN)
+        volatile_tickers_done_lst_window_length = volatile_tickers_done['window_length'].tolist()
+        volatile_tickers_done_lst_mcr = volatile_tickers_done['mcr'].tolist()
+        volatile_tickers_done_lst_mcr.append(np.NaN)
+        volatile_tickers_done_lst_window_length.append(np.NaN)
+        df_temp = pd.DataFrame({'tickers':volatile_tickers_done_lst,'window_length':volatile_tickers_done_lst_window_length,'margin':volatile_tickers_done_lst_margin,'mcr':volatile_tickers_done_lst_mcr})
+        df_temp.to_csv('tickers_done_short_correct.csv')
+    
     
     
 #    df_test[ticker] = best_investment_dev
