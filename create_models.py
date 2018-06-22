1d88ae5899d8138d1d45b84e5168e2b68248742b
Howard Riddiough <howardriddiough@howworkmac.home>
2018-06-22 19:27:35 +0200

    reworked py file to only create models not found in shotterm_models amoung other changes

--- create_models.py
+++ create_models.py
@@ -1,36 +1,27 @@
-import os
+import datetime as dt
 
 import pandas as pd
 import numpy as np
-from numpy import newaxis
 
 import yahoo_reader
 import preprocessing as pp
 import lstm_utils as utils
 import lstm_model
-import plotting
 import gc
-#import pygmail
-from keras.models import Sequential, load_model
 
-industry = 'pharmaceuticals'
-results = './'+industry+'/results/'
-tickers = './'+industry+'/tickers/'
-volatile_tickers = pd.read_csv(tickers+industry+'.csv',sep=',')
-#volatile_tickers= volatile_tickers.reset_index()
-volatile_tickers_list = volatile_tickers['Ticker'].tolist()
+user = utils.load_user_from_yml(yml_file='./configs/user_settings.yml')
+tickers_lst = utils.get_tickers_for_a_user(user=user)
+tickers_done = utils.get_tickers_done('./shortterm_models/')
+tickers_lst = [ticker for ticker in tickers_lst if ticker not in tickers_done]
 
+yr = yahoo_reader.finance_data(tickers=tickers_lst)
+df_main, tickers = yr.get_fix_yahoo_data()
 
-shortterm_models = './'+industry+'/shortterm_models/'
-
-#volatile_tickers_to_complete = [item for item in volatile_tickers_list if item not in volatile_tickers_done_list]
-yr = yahoo_reader.finance_data(tickers=volatile_tickers_list)
-df_main = yr.get_fix_yahoo_data()
-df_main = df_main[0]
 days_ahead=1
-df_test = {}
 compare_investment = 300.
-for ticker in volatile_tickers_list:
+for ticker in tickers:
+    results_dict = {}
+    results_df = utils.read_results_csv(directory='./results/')
     for window_length in [16]:
         df = df_main[df_main.ticker == ticker].reset_index(drop=True)
         df['volume'] = df['volume'].replace(0,1.0)
@@ -46,36 +37,55 @@ for ticker in volatile_tickers_list:
         #closing price must go first 
         combined_input = np.concatenate((close_nmd_array,open_nmd_close_array,low_nmd_close_array,high_nmd_close_array,volumne_nmd_array, day_number_array),axis=2)
         x_train, y_train, x_test, y_test, train_days, test_days, test_windows,train_windows_non_randomized,x_train_sim = utils.train_test_split(combined_input,combined_input.shape[2], dates_array, windows_array)
-        investment, best_investment_dev,params,margin,mcr = lstm_model.randomised_model_config(test_windows,
-                                                        df_p,
-                                                        test_days,
-                                                        train_days,
-                                                        train_windows_non_randomized,
-                                                        x_train_sim,
-                                                        combined_input.shape[2],
-                                                        window_length,
-                                                        ticker,
-                                                        df,
-                                                        days_ahead,
-                                                        x_train,
-                                                        y_train, 
-                                                        x_test,
-                                                        y_test,
-                                                        industry,
-                                                        iterations=15)
-        gc.collect()    
-        if (investment/compare_investment)>1.00 :  
-            volatile_tickers = pd.read_csv(tickers+industry+'.csv',sep=',')
-            volatile_tickers['margin'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = margin
-            volatile_tickers['window_length'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = window_length
-            volatile_tickers['mcr'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = mcr
-            volatile_tickers.to_csv(tickers+industry+'.csv')
-        elif (investment/compare_investment)<1.00 :  
-            volatile_tickers = pd.read_csv(tickers+industry+'.csv',sep=',')
-            volatile_tickers['margin'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = np.NaN
-            volatile_tickers['window_length'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = np.NaN
-            volatile_tickers['mcr'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = np.NaN
-            volatile_tickers.to_csv(tickers+industry+'.csv')
+        investment, best_investment_dev, params, margin, mcr = lstm_model.randomised_model_config(
+            test_windows,
+            df_p,
+            test_days,
+            train_days,
+            train_windows_non_randomized,
+            x_train_sim,
+            combined_input.shape[2],
+            window_length,
+            ticker,
+            df,
+            days_ahead,
+            x_train,
+            y_train, 
+            x_test,
+            y_test,
+            iterations=2
+        )
+        
+        gc.collect()   
+        
+        if (investment/compare_investment) > 1.00:
+            results_dict['margin'] = [np.round(margin, 2),]
+            results_dict['window_length'] = [window_length,]
+            results_dict['mcr'] = [mcr,]
+            results_dict['ticker'] = [ticker,]
+            results_dict['date_created'] = [dt.datetime.now(),]
+            
+#            volatile_tickers = pd.read_csv(tickers+industry+'.csv',sep=',')
+#            volatile_tickers['margin'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = margin
+#            volatile_tickers['window_length'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = window_length
+#            volatile_tickers['mcr'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = mcr
+#            volatile_tickers.to_csv(tickers+industry+'.csv')
+        elif (investment/compare_investment) < 1.00:  
+            results_dict['margin'] = [np.NaN,]
+            results_dict['window_length'] = [np.NaN,]
+            results_dict['mcr'] = [np.NaN,]
+            results_dict['ticker'] = [ticker,]
+            results_dict['date_created'] = [dt.datetime.now(),]
+        
+        results_df = pd.concat([results_df, pd.DataFrame(results_dict)])
+        results_df.to_csv('./results/model_results.csv', index=False)
+
+            
+#            volatile_tickers = pd.read_csv(tickers+industry+'.csv',sep=',')
+#            volatile_tickers['margin'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = np.NaN
+#            volatile_tickers['window_length'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = np.NaN
+#            volatile_tickers['mcr'][volatile_tickers.loc[volatile_tickers['Ticker'] == ticker].index[0]] = np.NaN
+#            volatile_tickers.to_csv(tickers+industry+'.csv')
             
     
 
