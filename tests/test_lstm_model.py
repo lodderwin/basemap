b86ea605b5851f76d6d96e4d594c656e85dda3e8
Howard Riddiough <riddiough_h@schiphol.nl>
2018-07-28 14:33:51 +0200

    added new tests for lstm model

--- tests/test_lstm_model.py
+++ tests/test_lstm_model.py
@@ -1,17 +1,37 @@
 import pytest
+import numpy as np
 
+from pynt.yahoo_reader import FinanceData
 from pynt.lstm_model import LstmModel
-from pynt.preprocessing import PreProcessing as pp
-from tests.test_utils import Utils as U
 
 
-def test_lstm_model():
-    fd = U.init_financedata()
-    fd.get_fix_yahoo_data()
-    df = pp.pre_process_data(fd.df)
+# def test_window_length():
+#     fd = FinanceData(start_date='2001-01-01', end_date='2001-02-01',
+#                      tickers=['AAPL'], data_dir='./data/')
+#
+#     lstm = LstmModel(df=fd.df_processed, ticker='AAPL')
+#
+#     assert type(lstm.window_length) == int
 
-    # for col in df.columns:
-    #     print(col)
-    # print(df.head())
+# def test_nmd_array_dict():
+#     fd = FinanceData(start_date='2001-01-01', end_date='2001-02-01',
+#                      tickers=['AAPL'], data_dir='./data/')
+#
+#     lstm = LstmModel(df=fd.df_processed, ticker='AAPL')
+#
+#     assert type(lstm.nmd_array_dict) == dict
+#     for array in lstm.nmd_array_dict:
+#         type(lstm.nmd_array_dict[array]) == np.array
+#         len(lstm.nmd_array_dict[array]) > 0
 
-    LstmModel(df=df, ticker='AAPL')
+
+def test_train_test_split():
+    fd = FinanceData(start_date='2001-01-01', end_date='2001-02-01',
+                         tickers=['AAPL'], data_dir='./data/')
+
+    lstm = LstmModel(df=fd.df_processed, ticker='AAPL')
+    lstm.train_test_split()
+
+    # print(lstm.X_train)
+    assert len(lstm.X_train) > 0
+    assert len(lstm.y_train) > 0
