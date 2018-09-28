df3bd1389acc54479a5689f201dc84ce12bc192a
Frank <frankbrouwer@live.nl>
2018-09-28 10:13:21 +0200

    new function for adding large-decrease data

--- sameindustrytickers.py
+++ sameindustrytickers.py
@@ -8,6 +8,7 @@ import pandas as pd
 import yahoo_reader
 import lstm_utils as utils
 import random
+import numpy as np
 
 #%% usual stuff from create_models.py
 user = utils.load_user_from_yml(yml_file='./configs/user_settings.yml')
@@ -46,5 +47,53 @@ def get_same_industry_tickers(ticker, samplesize):
     return df_sameindustrytickers
 
 #%%
+def get_large_decreases_in_industry(ticker, percentage):
+    
+    #create usefull string for ticker    
+    ticker = str(ticker)
+    for character in ["[","]","'"]:
+        if character in str(ticker):
+            ticker = ticker.replace(character,"")
+
+    #create list of all tickers in same industry
+    df_alltickers = pd.read_csv('./tickers.csv')
+    industry = df_alltickers[df_alltickers.ticker == ticker].iloc[0]['industry']
+    industrytickers = df_alltickers[df_alltickers.industry == industry]['ticker'].tolist()
+
+    #for counter
+    number = len(industrytickers)
+
+    #main dataframe
+    df_largedecreases = pd.DataFrame()
+    
+    #dowload and append data for each ticker (one-by-one to avoid Frank's error)
+    for sit in industrytickers:
+        try:
+            print("Downloading data for " + str(number) + " same-industry tickers.")
+            sit = [sit]
+            yr = yahoo_reader.finance_data(tickers=sit)
+            df, tickers = yr.get_fix_yahoo_data()
+            
+            #calculate decrease and select 100 rows above and 50 rows below rows where this decrease is larger than 'percentage'
+            df['perc_change'] = df.close/df.open
+            df['arounddecrease'] = 0
+            for X in list(range(-100,50)):
+                df['arounddecrease'] = np.where(df.perc_change.shift(X)<percentage, 1, df.arounddecrease)
+            
+            #append ticker data to main dataframe
+            df_largedecreases = df.append(df, ignore_index=True)
+            number = number-1
+            print("Ticker data added") 
+        except: 
+            number = number-1
+            print("No data")  
+    
+    df_largedecreases = df_largedecreases[df_largedecreases.arounddecrease == 1]
+    df_largedecreases = df_largedecreases.drop(columns=['arounddecrease', 'perc_change'])
+    
+    return df_largedecreases
+#%%
+get_large_decreases_in_industry(tickers_to_do, 0.95)
 
 
+#%%
