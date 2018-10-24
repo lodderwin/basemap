0b30d352b06a5821e885b09de657e4db040cf8b6
Frank <frankbrouwer@live.nl>
2018-10-24 15:15:59 +0200

    added get_sample function

--- sameindustrytickers.py
+++ sameindustrytickers.py
@@ -16,8 +16,137 @@ user_tickers = utils.get_tickers_for_a_user(user=user)
 tickers_done = utils.get_tickers_done('./results/')
 tickers_to_do = [ticker for ticker in user_tickers if ticker not in tickers_done]
 #tickers_to_do = tickers_to_do[:1]
+
+
 #%% 
 
+def get_sample(ticker, vol_samplesize, vol_window, decrease_percentage):
+    
+    #download data for ticker A
+    yr = yahoo_reader.finance_data(tickers=[ticker])
+    dfA, tickersA = yr.get_fix_yahoo_data()    
+    
+    #calculate price and volatility (average standarddeviation over window) and save it for the last day
+    dfA['volatility'] = dfA.close.rolling(vol_window).std()
+    volatilityA = dfA.volatility.tail(1)
+
+    #create usefull string for ticker    
+    ticker = str(ticker)
+    for character in ["[","]","'"]:
+        if character in str(ticker):
+            ticker = ticker.replace(character,"")
+
+    #create list of all tickers in same industry
+    df_alltickers = pd.read_csv('./tickers.csv')
+    industry = df_alltickers[df_alltickers.ticker == ticker].iloc[0]['industry']
+    industrytickers = df_alltickers[df_alltickers.industry == industry]['ticker'].tolist()
+    industrytickers.remove(ticker)
+
+    #for counter
+    number = len(industrytickers)
+    
+    #main dataframe and dataframe for last day of dataframe
+    df_industrytickers = pd.DataFrame()
+    df_tails = pd.DataFrame()
+    
+    #dowload and append data for each ticker (one-by-one to avoid Frank's error)
+    for sit in industrytickers:
+        try:
+            print("Downloading data for " + str(number) + " same-industry tickers.")
+            sit = [sit]
+            yr = yahoo_reader.finance_data(tickers=sit)
+            df, tickers = yr.get_fix_yahoo_data()
+            
+            #calculate volatility
+            df['volatility'] = df.close.rolling(vol_window).std()
+            
+            #take the last day of dataframe and compare volatility with that of ticker A
+            df_tail = df.tail(1)
+            volatility = df_tail.volatility.tail(1)
+            df_tail['vol_diff'] = abs(volatilityA - volatility)
+            df_tails = df_tails.append(df_tail, ignore_index=True)
+            
+            #calculate decrease and select 100 rows above and 50 rows below rows where this decrease is larger than 'percentage'
+            df['perc_change'] = df.close/df.open
+            df['arounddecrease'] = 0
+            for X in list(range(-100,50)):
+                df['arounddecrease'] = np.where(df.perc_change.shift(X)<decrease_percentage, 1, df.arounddecrease)
+            
+            #append rows around decreases to df_largedecreases and all rows to df_industrytickers
+            df_industrytickers = df_industrytickers.append(df, ignore_index=True)
+            number = number-1
+            print("Ticker data added") 
+            
+        except: 
+            number = number-1
+            print("No data")  
+
+    #sort the tickers by the difference in volatility compared to ticker A
+    df_tails = df_tails.sort_values('vol_diff', ascending=True) 
+    df_tails = df_tails.head(vol_samplesize)
+    sample = list(df_tails.ticker.unique())
+    print(sample)
+    
+    #make final dataframe with all data from stocks in sample
+    df_sample = pd.DataFrame()
+    for ticker in sample:
+        df = df_industrytickers[(df_industrytickers.ticker == ticker) | (df_industrytickers.arounddecrease == 1)]
+        df_sample = df_sample.append(df, ignore_index=True)
+
+    return df_sample
+
+
+#%%
+#df_sample = get_sample('BGI', vol_samplesize=2, vol_window=3, decrease_percentage=0.8)
+
+#%% 
+def get_large_decreases_in_industry(ticker, percentage):
+    
+    #create usefull string for ticker    
+    ticker = str(ticker)
+    for character in ["[","]","'"]:
+        if character in str(ticker):
+            ticker = ticker.replace(character,"")
+
+    #create list of all tickers in same industry
+    df_alltickers = pd.read_csv('./tickers.csv')
+    industry = df_alltickers[df_alltickers.ticker == ticker].iloc[0]['industry']
+    industrytickers = df_alltickers[df_alltickers.industry == industry]['ticker'].tolist()
+    industrytickers.remove(ticker)
+    
+    #for counter
+    number = len(industrytickers)
+
+    #main dataframe
+    df_largedecreases = pd.DataFrame()
+    
+    #dowload and append data for each ticker (one-by-one to avoid Frank's error)
+    for sit in industrytickers:
+        try:
+            print("Downloading data for " + str(number) + " same-industry tickers.")
+            sit = [sit]
+            yr = yahoo_reader.finance_data(tickers=sit)
+            df, tickers = yr.get_fix_yahoo_data()
+            
+            #calculate decrease and select 100 rows above and 50 rows below rows where this decrease is larger than 'percentage'
+            df['perc_change'] = df.close/df.open
+            df['arounddecrease'] = 0
+            for X in list(range(-100,50)):
+                df['arounddecrease'] = np.where(df.perc_change.shift(X)<percentage, 1, df.arounddecrease)
+            
+            #append ticker data to main dataframe
+            df_largedecreases = df_largedecreases.append(df, ignore_index=True)
+            number = number-1
+            print("Ticker data added") 
+        except: 
+            number = number-1
+            print("No data")  
+    
+    df_largedecreases = df_largedecreases[df_largedecreases.arounddecrease == 1]
+    df_largedecreases = df_largedecreases.drop(columns=['arounddecrease', 'perc_change'])
+    
+    return df_largedecreases
+#%%
 #function returns a random sample of 'samplesize' tickers for each industry
 #accounts for number of tickers already trained in an industry
 
